ce546a8ccc45daf7b36f16bf0600963097e16f28
zuiyu1998 <1542844298@qq.com>
2024-01-16 14:31:53 +0800

    refactor: 调整目录结构

--- src/helper/mod.rs
+++ src/helper/mod.rs
@@ -1,5 +1,3 @@
 mod jwt_helper;
-mod permission_helper;
 
 pub use jwt_helper::*;
-pub use permission_helper::*;
--- src/services/mod.rs
+++ src/services/mod.rs
@@ -1,7 +1,7 @@
 mod label_service;
-mod setting_service;
+mod project_setting;
 mod user_service;
 
 pub use label_service::*;
-pub use setting_service::*;
+pub use project_setting::*;
 pub use user_service::*;
