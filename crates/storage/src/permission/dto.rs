ab67c718fe2c36cac18bfcb2151ec706fafdb8d1
zuiyu1998 <1542844298@qq.com>
2024-01-16 13:56:38 +0800

    feat: 添加role实体

--- crates/storage/src/permission/dto.rs
+++ crates/storage/src/permission/dto.rs
@@ -1,18 +1,35 @@
+use rc_entity::chrono::NaiveDateTime;
 use std::collections::HashSet;
 
 #[derive(Debug, PartialEq, Eq, Hash)]
 pub struct Permission {
-    name: String,
+    pub id: i32,
+    pub name: String,
+    pub create_at: NaiveDateTime,
+    pub update_at: NaiveDateTime,
 }
 
-pub struct UserPermissions(HashSet<Permission>);
+impl PartialEq<&str> for Permission {
+    fn eq(&self, other: &&str) -> bool {
+        if self.name == **other {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+
+pub struct UserPermissions(HashSet<String>);
 
 impl UserPermissions {
     pub fn has_permission(&self, permission: &str) -> bool {
-        let permission = Permission {
-            name: permission.to_string(),
-        };
-
-        self.0.contains(&permission)
+        self.0.contains(permission)
     }
+}
+
+pub struct Role {
+    pub id: i32,
+    pub name: String,
+    pub create_at: NaiveDateTime,
+    pub update_at: NaiveDateTime,
 }
